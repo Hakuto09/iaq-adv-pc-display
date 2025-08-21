7b227dd96c051f1545e01f56c5c3078027721073
honma yuki/本間　祐樹 <honma-y@hakuto.co.jp>
2025-08-21 12:54:39 +0900

    20250821r01-003_calc-iaqdata

--- main.js
+++ main.js
@@ -96,50 +96,50 @@ function createWindow() {
   return win;
 }
 
+function getSendData(manufacturerData) {
+  const sendData = {
+    temperature: 0,
+    humidity: 0,
+    co2: 0,
+    tvoc: 0,
+    co: 0,
+    pm1_0: 0,
+    pm2_5: 0,
+    pm10: 0,
+    ch2o: 0,
+  };
+
+  sendData.temperature = calcTemperatureFrom16Bit(
+    (manufacturerData[0] << 8) | manufacturerData[1]
+  );
+  sendData.humidity = calcHumidityFrom16Bit(
+    (manufacturerData[2] << 8) | manufacturerData[3]
+  );
+  sendData.pm1_0 = calcPm1_0From16Bit(
+    (manufacturerData[4] << 8) | manufacturerData[5]
+  );
+  sendData.pm2_5 = calcPm2_5From16Bit(
+    (manufacturerData[6] << 8) | manufacturerData[7]
+  );
+  sendData.pm10 = calcPm10From16Bit(
+    (manufacturerData[8] << 8) | manufacturerData[9]
+  );
+  sendData.co2 = calcCO2From16Bit(
+    (manufacturerData[10] << 8) | manufacturerData[11]
+  );
+  sendData.tvoc = calcTVOCFrom16Bit(
+    (manufacturerData[12] << 8) | manufacturerData[13]
+  );
+  sendData.ch2o = calcCH2OFrom16Bit(
+    (manufacturerData[14] << 8) | manufacturerData[15]
+  );
+  sendData.co = calcCOFrom8Bit(manufacturerData[16]);
+
+  return sendData;
+}
+
 function setupBleWatchMacFilter(win) {
   let isScanning = false;
-
-  function getSendData(manufacturerData) {
-    const sendData = {
-      temperature: 0,
-      humidity: 0,
-      co2: 0,
-      tvoc: 0,
-      co: 0,
-      pm1_0: 0,
-      pm2_5: 0,
-      pm10: 0,
-      ch2o: 0,
-    };
-
-    sendData.temperature = calcTemperatureFrom16Bit(
-      (manufacturerData[0] << 8) | manufacturerData[1]
-    );
-    sendData.humidity = calcHumidityFrom16Bit(
-      (manufacturerData[2] << 8) | manufacturerData[3]
-    );
-    sendData.pm1_0 = calcPm1_0From16Bit(
-      (manufacturerData[4] << 8) | manufacturerData[5]
-    );
-    sendData.pm2_5 = calcPm2_5From16Bit(
-      (manufacturerData[6] << 8) | manufacturerData[7]
-    );
-    sendData.pm10 = calcPm10From16Bit(
-      (manufacturerData[8] << 8) | manufacturerData[9]
-    );
-    sendData.co2 = calcCO2From16Bit(
-      (manufacturerData[10] << 8) | manufacturerData[11]
-    );
-    sendData.tvoc = calcTVOCFrom16Bit(
-      (manufacturerData[12] << 8) | manufacturerData[13]
-    );
-    sendData.ch2o = calcCH2OFrom16Bit(
-      (manufacturerData[14] << 8) | manufacturerData[15]
-    );
-    sendData.co = calcCOFrom8Bit(manufacturerData[16]);
-
-    return sendData;
-  }
 
   ipcMain.on("startScan", (event, macAddress) => {
     if (isScanning) {
